845c1f80a983619fafe2b135b6fcca157449a904
albretsen <bjellanda@gmail.com>
2024-05-03 10:30:17 +0200

    Modularized the contact type

--- types/Contact.ts
+++ types/Contact.ts
@@ -1,39 +1,34 @@
+export type Phone = {
+    ID: string;
+    CountryCode: string;
+    Number: string;
+};
+
+export type Email = {
+    ID: string;
+    EmailAddress: string;
+};
+
+export type Address = {
+    ID: string;
+    AddressLine1: string;
+    AddressLine2?: string;
+    AddressLine3?: string;
+    City: string;
+    Country: string;
+    PostalCode: string;
+    Region: string;
+};
+
+export type ContactInfo = {
+    Name: string;
+    ID?: string;
+    DefaultPhone: Phone;
+    DefaultEmail: Email;
+    InvoiceAddress: Address;
+};
+
 export type Contact = {
     ID: string;
-    /*InfoID: number;*/
-    Info: {
-        /*DefaultEmailID: number;
-        DefaultPhoneID: number;
-        InvoiceAddressID: number;*/
-        Name: string;
-        ID?: string;
-        DefaultPhone: {
-            ID: string;
-            /*BusinessRelationID: number;*/
-            CountryCode: string;
-            /*Description: string;*/
-            Number: string;
-            /*Type: string;*/
-        };
-        DefaultEmail: {
-            ID: string;
-            /*BusinessRelationID: number;
-            Deleted: boolean;
-            Description: string | null;*/
-            EmailAddress: string;
-        };
-        InvoiceAddress: {
-            ID: string;
-            AddressLine1: string;
-            AddressLine2?: string;
-            AddressLine3?: string;
-            //BusinessRelationID: number;
-            City: string;
-            Country: string;
-            //CountryCode: string;
-            PostalCode: string;
-            Region: string;
-        };
-    };
-    /*Comment: string;*/
+    Info: ContactInfo;
 };
